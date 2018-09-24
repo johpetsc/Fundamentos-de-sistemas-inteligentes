bb5197445a5d75e5ab505dac949da7b765b6da1d
johpetsc <johpetsc@gmail.com>
2018-09-24 16:15:26 -0300

    codigo mais limpo

--- trab2.py
+++ trab2.py
@@ -1,34 +1,57 @@
 import pandas as pd
-import numpy as np
 from sklearn import datasets
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import cross_val_score, cross_val_predict
 from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
 import matplotlib.pyplot as plt
+import sys
 
 #https://towardsdatascience.com/random-forest-in-python-24d0893d51c0
+#Leitura do arquivo .csv usando a biblioteca pandas
 folhas = pd.read_csv("imagens/leaf.csv", header=None)
-print(folhas.head(340))
-labels = folhas.pop(0).values
+labels = folhas.pop(0).values 
 images = folhas.values
 
 #http://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html
+#Determinando os valores da floresta utilizando a biblioteca sklearn
 rf = RandomForestClassifier(n_estimators=50)
 fit = rf.fit(images,labels)
-predict = cross_val_predict(rf, images, labels)
+predict = cross_val_predict(rf, images, labels, cv = 10)
+score = cross_val_score(rf, images, labels, cv = 10)
 prob = rf.predict_proba(images)
+
+#Limpa os warnings da tela
+print("\033[H\033[J")
+
+print("\n\n\n\n\nValores das folhas:\n")
+print(folhas.head(340))
+sys.stdin.read(1)
+print("\n\nProbabillidade:\n")
 print(prob)
+sys.stdin.read(1)
+print("\n\nPrevisão validação cruzada:\n")
+print(predict)
+sys.stdin.read(1)
+print("\n\nValor validação cruzada:\n")
+print(score)
+sys.stdin.read(1)
+print("\n\nCaminho de decisão da árvore\n")
 print(rf.decision_path(images))
-print (accuracy_score(labels, predict))
+sys.stdin.read(1)
+print("\n\nPrecisão média:\n")
+print(accuracy_score(labels, predict))
+sys.stdin.read(1)
+print("\n\nMatriz de confusão:\n")
+print(confusion_matrix(labels, predict))
+sys.stdin.read(1)
+
+
 
 cm = confusion_matrix(labels, predict)
 plt.matshow(cm)
 plt.ylabel('X')
 plt.xlabel('Y')
-plt.title('Confusion matrix')
+plt.title('MATRIZ DE CONFUSAO')
 plt.colorbar()
 plt.show()
-
-
-#print(confusion_matrix(labels, y_pred))
